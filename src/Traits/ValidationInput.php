6468d672a7c42f5577a5f4f87725763413e9a84e
Iqbal Atma Muliawan <iqbalatma@gmail.com>
2023-12-05 13:53:44 +0700

    return back validate method on ValidationInput trait

--- src/Traits/ValidationInput.php
+++ src/Traits/ValidationInput.php
@@ -45,6 +45,22 @@ trait ValidationInput
         return $validatedData;
     }
 
+    /**
+     * Validates inputs.
+     * @param array $inputs
+     * @param array $rules
+     * @param array $messages
+     * @param array $attributes
+     *
+     * @return array
+     *
+     * @throws ValidationException
+     */
+    public function validate(array $inputs, array $rules, array $messages = [], array $attributes = []): array
+    {
+        return Validator::make($inputs, $rules, $messages, $attributes)->validate();
+    }
+
 
     /**
      * @param array $requestedData
